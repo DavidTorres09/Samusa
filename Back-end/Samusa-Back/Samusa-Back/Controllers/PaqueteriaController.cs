0447e8a7041d82d42826a4b0f09df1d4f7377cdf
Anto462 <0anthony0402@gmail.com>
2024-02-06 23:04:02 -0600

    Back End: Se agregan los controladores y las estructuras base para los archivos Data, luego se deben rellenar con los store procedure correspondientes o bien el metodo de Isercions, delete, etc a escoger

---
+++ Back-end/Samusa-Back/Samusa-Back/Controllers/PaqueteriaController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Samusa_Back.Data;
+using Samusa_Back.Models;
+
+namespace Samusa_Back.Controllers
+{
+    [ApiController]
+    [Route("api/samusa/paqueteria")]
+    public class PaqueteriaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public async Task<IActionResult> ViewPaquetes()
+        {
+            var paquetes = await PaqueteriasData.Read();
+
+            if (paquetes != null)
+            {
+                return Ok(paquetes);
+            }
+            else
+            {
+                return BadRequest(new { Status = 400, Message = "Error al obtener la lista de paquetes." });
+            }
+        }
+
+        [HttpGet]
+        [Route("listarUnico")]
+        public async Task<IActionResult> ViewPaqueteById(int idpaqSeguimiento)
+        {
+            var paquete = await PaqueteriasData.ReadOne(idpaqSeguimiento);
+
+            if (paquete != null)
+            {
+                return Ok(paquete);
+            }
+            else
+            {
+                return BadRequest(new { Status = 400, Message = "Error al obtener el paquete." });
+            }
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+        public async Task<IActionResult> SavePaquete([FromBody] Paqueterium paquete)
+        {
+            var confirmation = await PaqueteriasData.Create(paquete);
+            if (confirmation)
+            {
+                return Ok(new { Status = 200, Message = "Paquete guardado con éxito." });
+            }
+            else
+            {
+                return BadRequest(new { Status = 400, Message = "Error al guardar el paquete." });
+            }
+        }
+
+        [HttpDelete]
+        [Route("eliminar")]
+        public async Task<IActionResult> DeletePaquete(int idpaqSeguimiento)
+        {
+            var confirmation = await PaqueteriasData.Delete(idpaqSeguimiento);
+            if (confirmation)
+            {
+                return Ok(new { Status = 200, Message = "Paquete eliminado con éxito." });
+            }
+            else
+            {
+                return BadRequest(new { Status = 400, Message = "Error al eliminar el paquete." });
+            }
+        }
+
+        [HttpPut]
+        [Route("modificar")]
+        public async Task<IActionResult> ModifyPaquete([FromBody] Paqueterium paquete)
+        {
+            var confirmation = await PaqueteriasData.Update(paquete);
+            if (confirmation)
+            {
+                return Ok(new { Status = 200, Message = "Paquete modificado con éxito." });
+            }
+            else
+            {
+                return BadRequest(new { Status = 400, Message = "Error al modificar el paquete." });
+            }
+        }
+    }
+}
---
+++ Back-end/Samusa-Back/Samusa-Back/Data/RevisionAlmacenesData.cs
@@ -0,0 +1,53 @@
+﻿using Samusa_Back.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Samusa_Back.Data
+{
+    public class RevisionAlmacenesData
+    {
+
+        public static async Task<bool> Create(RevisionAlmacen revision)
+        {
+            using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+            {
+                return true;
+            }
+        }
+
+        public static async Task<bool> Update(RevisionAlmacen revision)
+        {
+            using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+            {
+                return true;
+            }
+        }
+
+        public static async Task<List<RevisionAlmacen>> Read()
+        {
+            List<RevisionAlmacen> RevsAlmacen = new List<RevisionAlmacen>();
+            using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+            {
+                return RevsAlmacen;
+            }
+        }
+
+        public static async Task<RevisionAlmacen> ReadOne(int IdformAlmacen)
+        {
+            RevisionAlmacen RevAlmacen = new RevisionAlmacen();
+            using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+            {
+                return RevAlmacen;
+            }
+        }
+
+        public static async Task<bool> Delete(int IdformAlmacen)
+        {
+            using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+            {
+                return true;
+            }
+        }
+
+    }
+}
