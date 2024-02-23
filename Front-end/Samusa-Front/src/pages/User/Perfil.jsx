06fac8282a9368c2d344efe47ce4feaac21005f3
Catfishman08 <101367701+Catfishman08@users.noreply.github.com>
2024-02-22 19:44:35 -0600

    Agregar formulario de usuario y link

---
+++ Front-end/Samusa-Front/src/pages/User/Perfil.jsx
@@ -0,0 +1,12 @@
+import ClientModal from "../../components/ClientModal"
+export default function Perfil() {
+
+
+    return(
+        
+        
+        <ClientModal></ClientModal>
+        
+        
+        )
+}
