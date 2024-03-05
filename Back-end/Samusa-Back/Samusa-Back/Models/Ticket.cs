617b8e4b13fad07a07e4814046148e7e85b8cedd
David Torres <angeldtm09@gmail.com>
2024-03-05 17:23:54 -0600

    Tickets (Not finished)
    -Add controllers of tickets
    -Add Sp's of tickets

---
+++ Back-end/Samusa-Back/Samusa-Back/Models/Ticket.cs
@@ -0,0 +1,31 @@
+﻿namespace Samusa_Back.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public string? Estado { get; set; }
+        public string? Prioridad { get; set; }
+        public string? Descripcion { get; set; }
+        public int IDCliente { get; set; }
+        public int IDColaborador { get; set; }
+    }
+
+    public class ViewTicket
+    {
+        public int TicketId { get; set; }
+        public string? Estado { get; set; }
+        public string? Prioridad { get; set; }
+        public string? DNI_CLIENTE { get; set; }
+        public string? DNI_COLABORADOR { get; set; }
+    }
+
+    public class TicketColaborador
+    {
+        public int TicketId { get; set; }
+        public string? Estado { get; set; }
+        public string? Prioridad { get; set; }
+        public string? Descripcion { get; set; }
+        public string? DNI_COLABORADOR { get; set; }
+    }
+}
+
