dabde72c6b679b97942c9d9a3b99e9c0e396a634
Anto462 <0anthony0402@gmail.com>
2024-03-16 00:17:19 -0600

    Front end: Estetica general de las tablas, se esta en pruebas con la tabla de cotizaciones inicialmente

--- Front-end/Samusa-Front/src/components/CotizaTable.jsx
+++ Front-end/Samusa-Front/src/components/CotizaTable.jsx
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import CotizaModal from './CotizaModal'; // Importa el componente del modal
 import "../css/Tables.css";
+import "../Css/datatables.min.css"
+import "../Css/datatables.css"
+
+import $ from 'jquery';
+import jszip from 'jszip';
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-autofill-dt';
+import 'datatables.net-buttons-dt';
+import 'datatables.net-buttons/js/buttons.colVis.mjs';
+import 'datatables.net-buttons/js/buttons.html5.mjs';
+import 'datatables.net-buttons/js/buttons.print.mjs';
+import 'datatables.net-colreorder-dt';
+import 'datatables.net-fixedcolumns-dt';
+import 'datatables.net-fixedheader-dt';
+import 'datatables.net-keytable-dt';
+import 'datatables.net-responsive-dt';
+import 'datatables.net-rowgroup-dt';
+import 'datatables.net-rowreorder-dt';
+import 'datatables.net-scroller-dt';
+import 'datatables.net-select-dt';
+window.JSZip = jszip;
+
 
 const CotizaTable = () => {
   const [isEditing, setIsEditing] = useState(false);
@@ -14,9 +36,31 @@ const CotizaTable = () => {
   useEffect(() => {
     fetch('https://localhost:7293/api/samusa/cotizacion/listar')
       .then(response => response.json())
-      .then(data => setTableData(data))
+      .then(data => {
+        setTableData(data);
+        // Asegúrate de que la tabla se inicialice solo después de que los datos estén listos
+        setTimeout(() => {
+          $(document).ready(function() {
+            $('#example').DataTable({
+              dom: 'Bfrtip',
+              destroy: true, // Agrega esta opción aquí
+              buttons: [
+                'copy', 'csv', 'excel', 'print'
+              ]
+            });
+          });
+        }, 0);
+      })
       .catch(error => console.error('Error fetching data:', error));
-  }, []);
+
+    // Esta parte de limpieza probablemente ya no sea necesaria con `destroy: true`
+    // pero depende de tu implementación específica y si tienes problemas de memoria
+    return () => {
+      if ($.fn.DataTable.isDataTable('#example')) {
+        $('#example').DataTable().destroy();
+      }
+    };
+}, []);
 
   const handleDelete = (idcotizacion) => {
     fetch(`https://localhost:7293/api/samusa/cotizacion/eliminar?idcotizacion=${idcotizacion}`, {
@@ -63,7 +107,7 @@ const CotizaTable = () => {
   return (
     <>
       <section className='data-table-section'>
-      <div className="table-container">
+      <div className="table-container col-12 mb-30">
         <h1 className="text-3xl font-bold my-4 text-gray-800">Tabla de cotizacion</h1>
 
         <div class="table-controls">
@@ -73,11 +117,11 @@ const CotizaTable = () => {
           >
             Agregar Cotizacion
           </button>
-          <input type="text" id="searchBox" placeholder='Buscar' className='search' onChange={(e) => SetQuery(e.target.value)} />
           </div>
-
-        <div className="">
-          <table className="Cliente-table w-full table-auto border-collapse rounded Tablebg">
+          
+          <div className="">
+          <div className="box-body">
+          <table id="example" className="display Cliente-table w-full table-auto border-collapse rounded Tablebg table table-bordered data-table data-table-export">
             <thead>
               <tr className="">
                 <th className="py-4 px-6">Id Cotizacion</th>
@@ -90,7 +134,7 @@ const CotizaTable = () => {
               </tr>
             </thead>
             <tbody>
-              {tableData.filter(item => item.producto.toLowerCase().includes(query)).map((item, index) => (
+              {tableData.map((item, index) => (
                 <tr key={index} className="border-b border-gray-200">
                   <td className="py-4 px-6">{item.idcotizacion}</td>
                   <td className="py-4 px-6">{item.idDni}</td>
@@ -107,6 +151,7 @@ const CotizaTable = () => {
             </tbody>
           </table>
         </div>
+          </div>
       </div>
       </section>
       {showEditModal && (<CotizaModal user={SelectedCotiza} onClose={handleCloseModal} isEditing={isEditing}/>      )}
--- Front-end/Samusa-Front/src/main.jsx
+++ Front-end/Samusa-Front/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../src/Css/Normalize.css'
+import '../src/Css/Template.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
