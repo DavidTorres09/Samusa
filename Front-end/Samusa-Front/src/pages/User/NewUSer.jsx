d5d499bf5dd2490fc52ff339abf8d2fc20dc92bd
David Torres <angeldtm09@gmail.com>
2024-06-12 23:45:48 -0600

    Update NewUSer.jsx

--- Front-end/Samusa-Front/src/pages/User/NewUSer.jsx
+++ Front-end/Samusa-Front/src/pages/User/NewUSer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import encryptionUtils from "../utilities/encryptionUtils";
+import encryptionUtils from "../../utilities/encryptionUtils";
 
 const NewUser = () => {
     const [usuario, setUsuario] = useState('');
