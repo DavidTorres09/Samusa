9d1692b94105b75f29d5f72f09e5812c0183958a
David Torres <angeldtm09@gmail.com>
2024-06-29 23:15:37 -0600

    Delete Old Back

--- Back-end/Samusa-Back/Samusa-Back/Data/Connection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Samusa_Back.Data
-{
-    public class Connection
-    {
-        //public static string connectionString = "Data Source=Angel;Initial Catalog=Prueba;Integrated Security=True;TrustServerCertificate=True";
-        public static string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SAMUSADB;Integrated Security=True;TrustServerCertificate=True";
-    }
-}
